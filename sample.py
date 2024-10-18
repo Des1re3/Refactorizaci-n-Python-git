eba013a6ab5e03eae4af9b129dfa8ff9e5c4ed1b
Desiree <db.estudiocontablepy@gmail.com>
2024-10-18 11:34:59 -0300

    mi tarea

--- sample.py
+++ sample.py
@@ -1,52 +1,42 @@
-def introducir_puntaje():
+def obtener_puntuacion_y_comentario():
     while True:
-        print( 'Por favor, introduzca una puntuación en una escala de 1 a 5' )
-        point = input()
-        if point.isdecimal():
-            point = int(point)
-            if  point <= 0 or point > 5:
-                print( 'Indíquelo en una escala de 1 a 5!!' )
+        puntuacion = input("Por favor, introduzca una puntuación (1-5): ")
+        if puntuacion.isdigit():
+            puntuacion = int(puntuacion)
+            if 1 <= puntuacion <= 5:
+                comentario = input("Por favor, introduzca un comentario: ")
+                return puntuacion, comentario
             else:
-                print( 'Introduzca sus comentarios.' )
-                comment = input()
-                ingresar_datos(point, comment)
-                break
-        else: 
-            print( 'Introduzca los puntos de valoración como números' )
+                print("Indíquelo en una escala de 1 a 5")
+        else:
+            print("Por favor, introduzca un número")
 
-def imprimir_resultados():
-    print( 'Resultados hasta la fecha.' )
-    read_file = open("data.txt", "r")
-    print( read_file.read() )
-    read_file.close()
-    
-def ingresar_datos(punto, comentario):
-    post = f'Punto: {punto} \t Comentario: {comentario}'
-    file_pc = open("data.txt", 'a')
-    file_pc.write( f'{ post } \n' )
-    file_pc.close()
+def guardar_datos(puntuacion, comentario):
+    with open("data.txt", "a") as file:
+        file.write(f"Puntuación: {puntuacion}, Comentario: {comentario}\n")
 
-
-#---------------
+def mostrar_resultados():
+    with open("data.txt", "r") as file:
+        print(file.read())
 
 while True:
-    print( 'Seleccione el proceso que desea aplicar' )
-    print( '1: Introduzca los puntos de valoración y los comentarios.' )
-    print( '2: Comprueba los resultados obtenidos hasta ahora.' )
-    print( '3: Terminación.' )
-    num = input()
+    print("Seleccione el proceso que desea aplicar:")
+    print("1: Ingresar puntuación y comentario")
+    print("2: Comprueba los resultados obtenidos hasta ahora.")
+    print("3: Finalizar")
 
-    if num.isdecimal():
-        num = int(num)
-
-        if num == 1:
-            introducir_puntaje()
-        elif num == 2:
-            imprimir_resultados()
-        elif num == 3:
-          print( 'Terminación.' )
-          break
+    opcion = input()
+    if opcion.isdigit():
+        opcion = int(opcion)
+        if opcion == 1:
+            puntuacion, comentario = obtener_puntuacion_y_comentario()
+            guardar_datos(puntuacion, comentario)
+        elif opcion == 2:
+            mostrar_resultados()
+        elif opcion == 3:
+            print("Finalizando")
+            break
         else:
-            print( 'Por favor, introduzca del 1 a 3' )
+            print("Introduzca un número del 1 al 3")
     else:
-        print( 'Por favor, introduzca del 1 a 3' )
+        print("Introduzca un número del 1 al 3")
